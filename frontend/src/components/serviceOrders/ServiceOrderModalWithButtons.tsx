30465e49ed62a49b576b89e1c506f04d2bf17164
Felipe <felipealencar.0014@gmail.com>
2026-06-16 14:17:42 -0300

    Adicionado modal de confirmação para os botões dentro de editar ordem: Cancelar, Atualizar e Finalizar

--- frontend/src/components/serviceOrders/ServiceOrderModalWithButtons.tsx
+++ frontend/src/components/serviceOrders/ServiceOrderModalWithButtons.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { BaseServiceOrderModal } from './BaseServiceOrderModal';
 import { Button } from '../form/Button';
 import {
-    ServiceOrderModalWithButtonsProps
-} from '@/types/modal/service_orders/ServiceOrderModalWithButtonsProps';
+    ServiceOrderModalWithButtonsProps,
+    ActionType
+} from '@/types/modal/service_order';
+import { ConfirmActionModal } from '../modal/ConfirmActionModal';
 
 export function ServiceOrderModalWithButtons({
     children,
@@ -17,84 +20,139 @@ export function ServiceOrderModalWithButtons({
     onUpdateOrder,
     onFinishOrder
 }: ServiceOrderModalWithButtonsProps) {
+    const [action, setAction] = useState<ActionType>(null);
+
+    function handleConfirm() {
+        switch (action) {
+            case 'cancel':
+                onCancelOrder?.();
+                break;
+
+            case 'update':
+                onUpdateOrder?.();
+                break;
+
+            case 'finish':
+                onFinishOrder?.();
+                break;
+        }
+
+        setAction(null);
+    }
+
+    const modalConfig = {
+        cancel: {
+            title: 'Cancelar Ordem',
+            description:
+                'Tem certeza que deseja cancelar esta ordem de serviço?',
+            confirmText: 'Cancelar Ordem'
+        },
+
+        update: {
+            title: 'Atualizar Ordem',
+            description:
+                'Deseja realmente atualizar esta ordem de serviço?',
+            confirmText: 'Atualizar Ordem'
+        },
+
+        finish: {
+            title: 'Finalizar Ordem',
+            description:
+                'Tem certeza que deseja finalizar esta ordem de serviço?',
+            confirmText: 'Finalizar Ordem'
+        }
+    };
+
     return (
-        <BaseServiceOrderModal
-            isOpen={isOpen}
-            onClose={onClose}
-        >
-            <div className="flex-1 overflow-auto px-6 pb-6">
-                {children}
-            </div>
-
-            {items?.length > 0 && (
-                <div className="
-                    p-6 flex
-                    md:justify-around
-                    justify-center
-                    gap-2
-                    border-t border-gray-300
-                ">
-                    <Button
-                        type="submit"
-                        onClick={onCancelOrder}
-                        disabled={isPending}
-                        className="
-                            w-64
-                            bg-gray-500
-                            text-white
-                            p-2
-                            rounded
-                            hover:bg-gray-600
-                            font-semibold
-                        "
-                    >
-                        {isPending
-                            ? `${optionTitle?.[0] ?? ""}`
-                            : `${optionTitle?.[1] ?? ""}`
-                        }
-                    </Button>
-
-                    <Button
-                        type="submit"
-                        onClick={onUpdateOrder}
-                        disabled={isPending}
-                        className="
-                            w-64
-                            bg-blue-500
-                            text-white
-                            p-2
-                            rounded
-                            hover:bg-blue-600
-                            font-semibold
-                        "
-                    >
-                        {isPending
-                            ? `${optionTitle1?.[0] ?? ""}`
-                            : `${optionTitle1?.[1] ?? ""}`
-                        }
-                    </Button>
-
-                    <Button
-                        type="submit"
-                        onClick={onFinishOrder}
-                        disabled={isPending}
-                        className="
-                            w-64
-                            bg-orange-500
-                            text-white
-                            p-2
-                            rounded
-                            hover:bg-orange-600
-                            font-semibold
-                        "
-                    >
-                        {isPending
-                            ? `${optionTitle2?.[0] ?? ""}`
-                            : `${optionTitle2?.[1] ?? ""}`
-                        }
-                    </Button>
+        <>
+            <BaseServiceOrderModal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <div className="flex-1 overflow-auto px-6 pb-6">
+                    {children}
                 </div>
-            )}
-        </BaseServiceOrderModal>
+
+                {items?.length > 0 && (
+                    <div className="
+                        p-6 flex
+                        md:justify-around
+                        justify-center
+                        gap-2
+                        border-t border-gray-300
+                    ">
+                        <Button
+                            type="submit"
+                            onClick={() => setAction('cancel')}
+                            disabled={isPending}
+                            className="
+                                w-64
+                                bg-gray-500
+                                text-white
+                                p-2
+                                rounded
+                                hover:bg-gray-600
+                                font-semibold
+                            "
+                        >
+                            {isPending
+                                ? `${optionTitle?.[0] ?? ""}`
+                                : `${optionTitle?.[1] ?? ""}`
+                            }
+                        </Button>
+
+                        <Button
+                            type="submit"
+                            onClick={() => setAction('update')}
+                            disabled={isPending}
+                            className="
+                                w-64
+                                bg-blue-500
+                                text-white
+                                p-2
+                                rounded
+                                hover:bg-blue-600
+                                font-semibold
+                            "
+                        >
+                            {isPending
+                                ? `${optionTitle1?.[0] ?? ""}`
+                                : `${optionTitle1?.[1] ?? ""}`
+                            }
+                        </Button>
+
+                        <Button
+                            type="submit"
+                            onClick={() => setAction('finish')}
+                            disabled={isPending}
+                            className="
+                                w-64
+                                bg-orange-500
+                                text-white
+                                p-2
+                                rounded
+                                hover:bg-orange-600
+                                font-semibold
+                            "
+                        >
+                            {isPending
+                                ? `${optionTitle2?.[0] ?? ""}`
+                                : `${optionTitle2?.[1] ?? ""}`
+                            }
+                        </Button>
+                    </div>
+                )}
+            </BaseServiceOrderModal>
+
+            <ConfirmActionModal
+                isOpen={!!action}
+                isPending={isPending}
+                onClose={() => setAction(null)}
+                onConfirm={handleConfirm}
+                title={action ? modalConfig[action].title : ''}
+                description={action ? modalConfig[action].description : ''}
+                confirmText={action ? modalConfig[action].confirmText : ''}
+            />
+        </>
     );
 }
---
+++ frontend/src/types/dashboard/service_order.ts
@@ -0,0 +1,85 @@
+import { ProductProps } from "./product";
+
+export interface ServiceOrdersProps {
+    id: number;
+    status: string;
+    total: number;
+    user: {
+        id: number;
+        name: string;
+    };
+    client: {
+        id: number;
+        name: string;
+    };
+    items: {
+        productId: number;
+        quantity: number;
+    }[];
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface ServiceOrdersResponseProps {
+    data: ServiceOrdersProps[];
+    meta: {
+        total: number;
+        page: number;
+        lastPage: number;
+        limit: number;
+    };
+}
+
+export interface BaseCardProps {
+    children: React.ReactNode
+    titleP: string;
+    className?: string;
+}
+
+export interface ItemProps {
+    product: ProductProps;
+    quantity: number;
+    price: number;
+}
+
+export interface ProductsListProps {
+    items: ItemProps[];
+    handleChangeQuantity: (id: number, value: string) => void;
+    handleRemoveProductList: (id: number) => void;
+}
+
+export interface EditItemProps {
+    items: [
+        {
+            productId: number;
+            quantity: number;
+        }
+    ]
+}
+
+export interface SelectProductProps {
+    value: string;
+    error: string;
+    products: ProductProps[];
+    onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export interface TotalsProps {
+    items: ItemProps[];
+    subtotal: number;
+    quantityProduct: number;
+}
+
+export interface ViewProductsListProps {
+    items: ItemProps[];
+}
+
+// Request
+export interface CreateRequest {
+    clientId: number;
+    items: {
+        productId: number,
+        quantity: number
+    }[];
+}
